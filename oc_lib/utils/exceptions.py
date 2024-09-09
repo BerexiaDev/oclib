29c52aaf5034a62e7f9c4c59c667ecaeafdc288e
Bouhm-Yassine <yassinbouhm@gmail.com>
2024-09-09 14:37:15 +0100

    moved string fcts + exceptions + paginator

--- oc_lib/utils/exceptions.py
+++ oc_lib/utils/exceptions.py
@@ -1,3 +1,22 @@
+class NotFoundError(Exception):
+    """Exception raised when required data is not available."""
+
+
+class DataUnavailableError(Exception):
+    """Exception raised when required data is not available."""
+
+
+class AlreadyExists(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class Unmodifiable(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+        
 class DateValidationError(Exception):
     def __init__(self, message):
         self.message = message
--- oc_lib/utils/strings.py
+++ oc_lib/utils/strings.py
@@ -1,5 +1,19 @@
+from datetime import datetime
 import importlib
 
 def get_class_instance(module_name, class_name):
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
+
+def date_now():
+    return datetime.date(datetime.now())
+
+def update_element(data, element):
+    for key, value in data.items():
+        setattr(element, key, value)
+    return element
+
+
+def convert_str_to_date(value):
+    return datetime.date(datetime.strptime(value, "%Y-%m-%d"))
+
