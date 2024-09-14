0c9f47eac770b263426388ad83f0fdc4ee44ed73
MANAVANA Pierroldin <remagnavana@gmail.com>
2024-09-14 15:41:54 +0100

    remove nature pm from beneficiaire pm

--- oc_lib/models/beneficiaire_pm.py
+++ oc_lib/models/beneficiaire_pm.py
@@ -10,6 +10,5 @@ class BeneficiairePm(db.Model, Repository):
     solde_disponible = db.Column(db.Float, nullable=False, default=0)
     qualite = db.Column(db.Integer, nullable=False)
     date_solde = db.Column(db.Date, nullable=False)
-    nature_pm = db.Column(db.Integer)
     nationalite = db.Column(db.String(120), nullable=True)
     idce = db.Column(db.String(50))
