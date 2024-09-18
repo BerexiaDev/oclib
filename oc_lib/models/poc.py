f2742ab95df69f3e2652f8ed85cdc1bc7907771f
ceddibao <bllllaraf55@gmail.com>
2024-09-18 07:27:05 -0400

    removed the dup seuil_encaisse aded

--- oc_lib/models/poc.py
+++ oc_lib/models/poc.py
@@ -23,7 +23,6 @@ class Poc(db.Model, Repository):
     is_permanent = db.Column(db.Boolean, nullable=False)
     numero_agrement = db.Column(db.String(50))
     seuil_encaisse = db.Column(db.Float)
-    seuil_encaisse = db.Column(db.Float, nullable=False, default=0)
     seuil_encaisse_depasse = db.Column(db.Boolean, default=False)  # Indicates if seuil_encaisse is exceeded
     flag_retablissement_agrement = db.Column(db.Boolean)
     date_retablissement_agrement = db.Column(db.Date)
