fd4138db85085afabedae0a1539d67c8bac23292
MANAVANA Pierroldin <remagnavana@gmail.com>
2024-09-18 19:05:42 +0100

    commit update autorisation particuliere dto, add unauthorized exception

--- oc_lib/models/autorisation_particuliere.py
+++ oc_lib/models/autorisation_particuliere.py
@@ -10,8 +10,7 @@ class AutorisationParticuliere(db.Model, Repository):
     montant_supplementaire = db.Column(db.Float,nullable=False, default = 0)
     numero_autorisation =db.Column(db.Integer, nullable=False)
     date_autorisation =db.Column(db.Date, nullable=False)
-    flag_desactivation_complement_dotation = db.Column(db.Boolean)
-    statut =db.Column(db.Integer, default = 1) # 1 Encours, 2 valide
+    statut = db.Column(db.Integer, default = 1) # 1 Encours, 2 valide
     created_by =db.Column(db.String(240),nullable=False, default='')
     date_creation = db.Column(db.Date, nullable=False, default=date_now())
     validated_by = db.Column(db.String(240))
