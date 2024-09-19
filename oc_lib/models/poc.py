6b21ae504e3c715a13a327d885019d2f36206188
Bouhm Yassine <57569395+Bouhm-Yassine@users.noreply.github.com>
2024-09-19 11:50:15 +0100

    Update poc.py

--- oc_lib/models/poc.py
+++ oc_lib/models/poc.py
@@ -31,7 +31,7 @@ class Poc(db.Model, Repository):
     date_debut_activite = db.Column(db.Date, nullable=False)
     date_fin_activite = db.Column(db.Date)
     statut_activite = db.Column(db.Integer, default=1, nullable=False)
-    statut_agrement = db.Column(db.Integer)
+    statut_agrement = db.Column(db.Integer, default=1, nullable=False)
     raison_sociale_pm = db.Column(db.String(100), nullable=False)
 
     creation_status = db.Column(db.Integer, default=0)
