697e0521b5d244ffc8ef7801d24ed9eeeb20a822
Bouhm-Yassine <yassinbouhm@gmail.com>
2024-09-20 19:51:56 +0100

    add save bulk logic

--- oc_lib/repository.py
+++ oc_lib/repository.py
@@ -152,3 +152,20 @@ class Repository:
                 db.session.expire_all()
             db.session.refresh(self)
         return self
+
+    @classmethod
+    def bulk_save(cls, objects, commit=True):
+        """
+        Save multiple objects to the database in bulk.
+        """
+        try:
+            # Add all objects to the session in bulk
+            db.session.bulk_save_objects(objects)
+            
+            if commit:
+                db.session.commit()
+            
+            return True  # Return True to indicate success
+        except Exception as e:
+            db.session.rollback()  # Rollback if something goes wrong
+            raise Exception(f"Error in bulk saving objects: {e}")
