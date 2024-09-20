91ca06d9dc2f7425e67e5869d55d90d623d08e34
myrzanur <hakedhacked0@gmail.com>
2024-09-20 12:40:28 +0200

    make optional

--- oc_lib/models/beneficiaire_pm.py
+++ oc_lib/models/beneficiaire_pm.py
@@ -4,8 +4,8 @@ from oc_lib.repository import Repository
 
 class BeneficiairePm(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    rc = db.Column(db.Integer, nullable=False)
-    centre = db.Column(db.Integer, nullable=False)
+    rc = db.Column(db.Integer)
+    centre = db.Column(db.Integer)
     raison_sociale =db.Column(db.String(100))
     solde_disponible = db.Column(db.Float, nullable=False, default=0)
     qualite = db.Column(db.String(240), nullable=False)
