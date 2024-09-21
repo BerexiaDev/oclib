2f3b715c699d8921f496c392cbacf5dc238afed1
MANAVANA Pierroldin <remagnavana@gmail.com>
2024-09-21 12:19:08 +0100

    rename rc to registre_commerce

--- oc_lib/models/beneficiaire_pm.py
+++ oc_lib/models/beneficiaire_pm.py
@@ -4,7 +4,7 @@ from oc_lib.repository import Repository
 
 class BeneficiairePm(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    rc = db.Column(db.Integer, nullable=False)
+    registre_commerce = db.Column(db.Integer, nullable=False)
     centre = db.Column(db.Integer,nullable=False)
     raison_sociale =db.Column(db.String(100))
     solde_disponible = db.Column(db.Float, nullable=False, default=0)
