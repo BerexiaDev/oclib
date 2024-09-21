65b8b50eddd6d153ea4719bbf25afb4a98b66518
ceddibao <bllllaraf55@gmail.com>
2024-09-21 10:16:20 -0400

    added cessionPocPmView to the init file

--- oc_lib/models/__init__.py
+++ oc_lib/models/__init__.py
@@ -54,6 +54,7 @@ from oc_lib.models.sous_operation import SousOperation
 from oc_lib.models.statut import Statut
 from oc_lib.models.suppleant import Suppleant
 from oc_lib.models.user import User
+from oc_lib.models.cession_poc_pm_view import CessionPocPmView
 
 
 # Association tables
