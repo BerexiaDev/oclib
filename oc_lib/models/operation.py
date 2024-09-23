216d72efe3984be043c8088316a40d6dc66b8ee8
MANAVANA Pierroldin <remagnavana@gmail.com>
2024-09-23 19:25:35 +0100

    refactor: add column created by, date creation devise label to operation table

--- oc_lib/models/operation.py
+++ oc_lib/models/operation.py
@@ -27,5 +27,11 @@ class Operation(db.Model, Repository):
     nom = db.Column(db.String(120))
     prenom = db.Column(db.String(120))
     raison_sociale =db.Column(db.String(100))
+    
+    created_by = db.Column(db.String(240), nullable=False)
+    devise_labels = db.Column(db.String(1000), nullable=False)
+    date_creation = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    
 
     __mapper_args__ = {"polymorphic_identity": "operation", "polymorphic_on": type_operation}
