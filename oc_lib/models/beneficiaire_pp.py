5de863077160eb6dcda9afaaa5691575a2e6d18d
myrzanur <hakedhacked0@gmail.com>
2024-09-23 16:41:55 +0200

    make beneficiaire pp fields nullable

--- oc_lib/models/beneficiaire_pp.py
+++ oc_lib/models/beneficiaire_pp.py
@@ -7,9 +7,9 @@ class BeneficiairePp(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True )
     nom = db.Column(db.String(120), nullable=False)
     prenom = db.Column(db.String(120), nullable=False)
-    nature_piece = db.Column( db.String(50), nullable=False )
-    numero_piece = db.Column( db.String(50), nullable=False )
-    nationalite = db.Column( db.String(120), nullable=True )
+    nature_piece = db.Column( db.String(50))
+    numero_piece = db.Column( db.String(50))
+    nationalite = db.Column( db.String(120))
     solde_disponible = db.Column(db.Float, nullable=False, default=0)
     qualite = db.Column(db.Integer, nullable=False)
     date_solde = db.Column(db.Date, nullable=True) #To be removed when we handle solde properly
