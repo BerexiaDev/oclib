e60b68ee7d3d5d7227d5532f5f3095471767ccba
Bouhm Yassine <57569395+Bouhm-Yassine@users.noreply.github.com>
2024-09-24 16:40:28 +0100

    Update operation_achat.py

--- oc_lib/models/operation_achat.py
+++ oc_lib/models/operation_achat.py
@@ -6,6 +6,6 @@ class OperationAchat(Operation):
     id = db.Column(db.Integer, db.ForeignKey("operation.id"), primary_key=True, nullable=False)
 
     numero_declaration = db.Column(db.String(240))
-    date_declaration = db.Column(db.Date, default=date_now())
+    date_declaration = db.Column(db.Date)
 
     __mapper_args__ = {"polymorphic_identity": "achat"}
