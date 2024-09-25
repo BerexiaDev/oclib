3d32ef4239ad541d34975518942daad1eff09bac
Bouhm Yassine <57569395+Bouhm-Yassine@users.noreply.github.com>
2024-09-25 16:28:15 +0100

    Update caisse_devise.py

--- oc_lib/models/caisse_devise.py
+++ oc_lib/models/caisse_devise.py
@@ -3,7 +3,7 @@ from oc_lib.repository import Repository
 
 class CaisseDevise(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(240), unique=True)  # Uniqueness constraint on label
+    label = db.Column(db.String(240))  # A poc shouldn't have duplicated currencies
     montant = db.Column(db.Float)
 
     poc_id = db.Column(db.Integer, db.ForeignKey('poc.id'))
