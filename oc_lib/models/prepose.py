271ff4f1b4ca24337cbefbff3f2af93127d75463
SoufianeAmghar <soufiane35.live@gmail.com>
2024-09-25 18:23:35 +0100

    Added poc backref for prepose

--- oc_lib/models/prepose.py
+++ oc_lib/models/prepose.py
@@ -7,5 +7,6 @@ class Prepose(Pp):
     __tablename__ = 'prepose'
     id = db.Column(db.Integer, db.ForeignKey('pp.id'), primary_key=True)
     poc_id = db.Column(db.Integer, db.ForeignKey('poc.id'))
+    poc = db.relationship('Poc', backref='prepose', uselist=False)
 
     __mapper_args__ = {'polymorphic_identity': 'prepose'}
