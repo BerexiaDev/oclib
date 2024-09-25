88c3ac417f46811e0270bc082c6cc5a2cde1ea87
myrzanur <hakedhacked0@gmail.com>
2024-09-25 17:45:27 +0200

    add cancelled_by, cancelled_by_id and date_cancellation to operation

--- oc_lib/models/operation.py
+++ oc_lib/models/operation.py
@@ -33,5 +33,8 @@ class Operation(db.Model, Repository):
     devise_labels = db.Column(db.String(1000), nullable=False)
     date_creation = db.Column(db.DateTime, default=datetime.utcnow)
     cancellation_reason = db.Column(db.String(240), nullable=False)
+    cancelled_by = db.Column(db.String(240), nullable=False)
+    cancelled_by_id = db.Column(db.Integer, nullable=False)
+    date_cancellation = db.Column(db.DateTime, default=datetime.utcnow)
  
     __mapper_args__ = {"polymorphic_identity": "operation", "polymorphic_on": type_operation}
