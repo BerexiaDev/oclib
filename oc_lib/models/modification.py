0829b6cd1eb02977ee34a48df1baf421885495a1
ceddibao <bllllaraf55@gmail.com>
2024-09-27 05:47:46 -0400

    add modifications to modification table

--- oc_lib/models/modification.py
+++ oc_lib/models/modification.py
@@ -1,13 +1,13 @@
 from oc_lib.repository import Repository
 from oc_lib.db import db
 from datetime import date
+from sqlalchemy.dialects.postgresql import JSONB
 
 
 class Modification(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
     motif = db.Column(db.String(300), unique=True)
-    object = db.Column(db.Integer)
-    attributs = db.Column(db.ARRAY(db.String(100)))
+    modifications = db.Column(JSONB)
     op_category = db.Column(db.Integer)
     motif_status = db.Column(db.Boolean, default=True)
     date_activation = db.Column(db.Date, default=date.today)
