8318d443f48cb633ce69aa78595ec8e7ab421d21
SoufianeAmghar <soufiane35.live@gmail.com>
2024-10-01 17:25:49 +0100

    Fixed pm foreign key

--- oc_lib/models/co_gerant_pm.py
+++ oc_lib/models/co_gerant_pm.py
@@ -4,6 +4,6 @@ from oc_lib.utils.events_decorator import register_event_listeners
 
 @register_event_listeners
 class CogerantPm(Pm):
-    id = db.Column(db.Integer, db.ForeignKey("pp.id"), primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey("pm.id"), primary_key=True)
     esd_id = db.Column(db.Integer, db.ForeignKey("esd.id"))
     __mapper_args__ = {"polymorphic_identity": "cogerantpm"}
