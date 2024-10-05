9515765a8e7b57d0aacbf6f15652d6b102d08240
ceddibao <bllllaraf55@gmail.com>
2024-10-05 08:46:51 -0400

    wrapped the associepm class with the event listenr

--- oc_lib/models/associe_pm.py
+++ oc_lib/models/associe_pm.py
@@ -1,7 +1,8 @@
 from oc_lib.db import db
 from oc_lib.models.pm import Pm
+from oc_lib.utils.events_decorator import register_event_listeners
 
-
+@register_event_listeners
 class AssociePm(Pm):
     id = db.Column(db.Integer, db.ForeignKey('pm.id'), primary_key=True)
     part_capital = db.Column(db.Integer)
