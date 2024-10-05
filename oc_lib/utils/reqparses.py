31f5f0a273dbecadbc0852f6191cc021056474c7
myrzanur <hakedhacked0@gmail.com>
2024-10-05 11:41:07 +0200

    choices

--- oc_lib/utils/reqparses.py
+++ oc_lib/utils/reqparses.py
@@ -6,5 +6,5 @@ def export_table_data_reqparse():
     parser.add_argument("sort_key", type=str, location="args", default="id")
     parser.add_argument("sort_order", type=int, location="args", default=1)
     parser.add_argument("table_name", type=str, location="args", required=True)
-    parser.add_argument("file_type", type=str, location="args", default="xlsx")  # xlsx, csv
+    parser.add_argument("file_type", type=str, location="args", default="xlsx", choices=["xlsx", "csv"])
     return parser
