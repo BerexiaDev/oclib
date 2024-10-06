38ee283b6fcdac37cb46bd4f0264dc74f5ba2d79
Bouhm-Yassine <yassinbouhm@gmail.com>
2024-10-06 15:54:06 +0100

    add some comments

--- oc_lib/models/beneficiaire.py
+++ oc_lib/models/beneficiaire.py
@@ -6,13 +6,13 @@ class Beneficiaire(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     qualite = db.Column(db.Integer, nullable=False)
     nationalite = db.Column( db.String(120))
-    date_solde = db.Column(db.Date, nullable=True) #To be removed when we handle solde properly
+    date_solde = db.Column(db.Date, nullable=True) # To be removed when we handle solde properly
     solde_disponible = db.Column(db.Float, nullable=False, default=0)
 
     poc_id = db.Column(db.Integer, db.ForeignKey('poc.id'))
-    numero_agrement = db.Column(db.String(50))
-    nom_agence = db.Column(db.String(50))
-    date_deactivation = db.Column(db.DateTime)
+    numero_agrement = db.Column(db.String(50)) # of the poc that did the changes
+    nom_agence = db.Column(db.String(50)) # of the poc that did the changes
+    date_deactivation = db.Column(db.DateTime) # date of deactivation
     statut = db.Column(db.Boolean, default=True, nullable=False)  # True active, False inactive
 
     # this will be the discriminator attribute
