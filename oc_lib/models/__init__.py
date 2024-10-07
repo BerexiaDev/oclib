837d1933aad5b94314bca7d4ef02f4061ba8e523
ceddibao <bllllaraf55@gmail.com>
2024-10-07 16:47:42 -0400

    added inactif pm to models.

--- oc_lib/models/__init__.py
+++ oc_lib/models/__init__.py
@@ -59,6 +59,7 @@ from oc_lib.models.statut import Statut
 from oc_lib.models.suppleant import Suppleant
 from oc_lib.models.user import User
 from oc_lib.models.inactif_pp import InactifPp
+from oc_lib.models.inactif_pm import InactifPm
 
 
 # Association tables
