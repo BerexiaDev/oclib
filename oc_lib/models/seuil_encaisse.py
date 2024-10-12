f58eebe6124232d7d1370f279f18e5670dc18693
Bouhm Yassine <57569395+Bouhm-Yassine@users.noreply.github.com>
2024-10-12 17:50:07 +0100

    Update seuil_encaisse.py

--- oc_lib/models/seuil_encaisse.py
+++ oc_lib/models/seuil_encaisse.py
@@ -6,7 +6,7 @@ from oc_lib.utils.strings import current_year
 class SeuilEncaisse(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
     annee =db.Column(db.String(4), nullable=False, default=current_year()) 
-    categorie_op = db.Column(db.Integer, nullable=False)
+    categorie_pc = db.Column(db.Integer, nullable=False)
     encaisse = db.Column(db.Float, nullable=False, default = 0)
     latence_jours = db.Column(db.Integer, nullable=False)
     latence_heure = db.Column(db.Integer, nullable=False) 
