65c89fa7a63cad13844f961e73e7bf96c347c602
Bouhm Yassine <57569395+Bouhm-Yassine@users.noreply.github.com>
2024-10-18 17:56:37 +0100

    Update demande_benef.py

--- oc_lib/models/demande_benef.py
+++ oc_lib/models/demande_benef.py
@@ -2,19 +2,18 @@ from oc_lib.db import db
 from oc_lib.repository import Repository
 from datetime import date
 
-
 class DemandeBenef(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Boolean)
+    decision = db.Column(db.Integer) # Statut (accepté/rejeté)
+    statut = db.Column(db.Integer) # Avancement (encours de validation, traité)
+    motif = db.Column(db.String)
+
     initiateur = db.Column(db.Integer)
-    created_by = db.Column(db.String(240), nullable=False, server_default="")
-    validateurs = db.Column(db.ARRAY(db.String(240)))
-    valide_oc = db.Column(db.Boolean, default=False)
+    created_by = db.Column(db.String(240), nullable=False)
+    validateur = db.Column(db.String(240))
     valide_manager_oc = db.Column(db.Boolean, default=False)
     benef_pp_id = db.Column(db.Integer, db.ForeignKey("beneficiaire_pp.id"))
     benef_pm_id = db.Column(db.Integer, db.ForeignKey("beneficiaire_pm.id"))
-    avancement = db.Column(db.Boolean, default=False)
     date_creation = db.Column(db.Date, default=date.today, onupdate=date.today)
 
-    change = db.relationship(
-        "Change", backref="demande_benef", lazy=True, uselist=False)
+    change = db.relationship("Change", backref="demande_benef", lazy=True, uselist=False)
