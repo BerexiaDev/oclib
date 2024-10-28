473a6b4e0c561b950871f7ddc5e8ed78abfcbb8c
ceddibao <bllllaraf55@gmail.com>
2024-10-28 19:02:42 -0400

    replaced avancement_statut with etape

--- oc_lib/models/demande_statut.py
+++ oc_lib/models/demande_statut.py
@@ -5,7 +5,7 @@ from datetime import date
 class DemandeStatut(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
     decision = db.Column(db.Integer) # Statut (accepté/rejeté)
-    avancement_statut = db.Column(db.Integer) # Avancement
+    etape = db.Column(db.Integer) # Avancement
 
     statut = db.Column(db.Integer, db.ForeignKey("statut.id"))
     
