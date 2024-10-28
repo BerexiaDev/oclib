920b9696838bf82e66f76c03c27cd3b0501c0d0f
Karim Bourass <bourasskarim9@gmail.com>
2024-10-28 15:57:23 +0100

    Add draft_reference

---
+++ oc_lib/models/draft_reference.py
@@ -0,0 +1,16 @@
+from oc_lib.repository import Repository
+from oc_lib.db import db
+
+
+class DraftReference(db.Model, Repository):
+  __tablename__ = 'draft_reference'
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  centre = db.Column(db.Integer, nullable=False)
+  ville = db.Column(db.String(50), nullable=False)
+  rc = db.Column(db.Integer)
+  raison_sociale = db.Column(db.String(100))
+  address = db.Column(db.String(200))
+  forme_juridique = db.Column(db.String(100))
+  capital_social = db.Column(db.Numeric(precision=20, scale=3))
+  date_creation = db.Column(db.Date, nullable=False)
+  date_radiation = db.Column(db.Date, nullable=True)
