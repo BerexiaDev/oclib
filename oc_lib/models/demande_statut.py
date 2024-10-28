a773a8acf36a388e6ce933e68176889bd4f444d4
ceddibao <bllllaraf55@gmail.com>
2024-10-28 17:39:35 -0400

    fix typo err.

--- oc_lib/models/demande_statut.py
+++ oc_lib/models/demande_statut.py
@@ -2,7 +2,7 @@ from oc_lib.db import db
 from oc_lib.repository import Repository
 from datetime import date
 
-class DemandeStatut(db.models, Repository):
+class DemandeStatut(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
     decision = db.Column(db.Integer) # Statut (accepté/rejeté)
     avancement_statut = db.Column(db.Integer) # Avancement
