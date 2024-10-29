442ff5372baf1d342e3d5089922340e375cf7790
ceddibao <bllllaraf55@gmail.com>
2024-10-29 16:35:12 -0400

    removed statut_id, added change jsonb to hold change data.

--- oc_lib/models/demande_statut.py
+++ oc_lib/models/demande_statut.py
@@ -1,6 +1,7 @@
 from oc_lib.db import db
 from oc_lib.repository import Repository
 from datetime import date
+from sqlalchemy.dialects.postgresql import JSONB
 
 class DemandeStatut(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,5 +9,5 @@ class DemandeStatut(db.Model, Repository):
     etape = db.Column(db.Integer) # Avancement
     date_creation = db.Column(db.Date, default=date.today, onupdate=date.today)
 
-    statut_id = db.Column(db.Integer, db.ForeignKey("statut.id"))
+    change = db.Column(JSONB)
     
