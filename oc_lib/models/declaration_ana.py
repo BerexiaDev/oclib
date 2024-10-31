90593ac4d91e764edb0861f5cf1e7fddc5fe59be
SoufianeAmghar <soufiane35.live@gmail.com>
2024-10-31 14:25:54 +0100

    Added declaration Ana model

---
+++ oc_lib/models/declaration_ana.py
@@ -0,0 +1,40 @@
+from oc_lib.repository import Repository
+from oc_lib.db import db
+
+class DeclarationAna(db.Model, Repository):
+    id = db.Column(db.Integer, primary_key=True)
+    nom_agence = db.Column(db.String(50))
+    adresse = db.Column(db.String(50), nullable=False)
+    localite = db.Column(db.String(50), nullable=False)
+    localite_code = db.Column(db.String(50), nullable=False)
+    region = db.Column(db.String(50), nullable=False)
+    is_agrement = db.Column(db.Boolean, default=False)
+    numero_agrement = db.Column(db.String(50))
+    seuil_encaisse = db.Column(db.Float)
+    date_debut_activite = db.Column(db.Date, nullable=False)
+    statut_activite = db.Column(db.Integer, default=1) #En activité par défaut
+    statut_agrement = db.Column(db.Integer, default=3) #Neant par défaut
+    date_debut_statut = db.Column(db.Date, nullable=False)
+    nom = db.Column(db.String(50))
+    prenom = db.Column(db.String(50))
+    nature_prepose = db.Column(db.String(50))
+    numero_piece = db.Column(db.String(50))
+    nationalite = db.Column(db.String(50))
+    adresse_prepose = db.Column(db.String(100))
+    email = db.Column(db.String)
+    phone = db.Column(db.String)
+    date_nomination = db.Column(db.Date) 
+    type_pm = db.Column(db.String)
+    created_by = db.Column(db.String(240), nullable=False, server_default="")
+    validator = db.Column(db.String)
+    statut = db.Column(db.Integer)
+    decision = db.Column(db.Integer)
+    
+    # One to one
+    scd_id = db.Column(db.Integer, db.ForeignKey("scd.id", ondelete="SET NULL"))
+    esd_id = db.Column(db.Integer, db.ForeignKey("esd.id", ondelete="SET NULL"))
+    ep_id = db.Column(db.Integer, db.ForeignKey("ep.id", ondelete="SET NULL"))
+    mandataire_id = db.Column(db.Integer, db.ForeignKey("mandataire.id", ondelete= "SET NULL"))
+    
+    lieu_implantation_id = db.Column(db.Integer, db.ForeignKey("lieu_implantation.id"))
+    lieu_implantation = db.relationship("LieuImplantation", backref="lieu_implantation")
