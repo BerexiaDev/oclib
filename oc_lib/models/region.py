56a9a3c826866dc658338d313c35cdc715f5b1e3
abdel jalil <abdeljalil.jabiri@berexia.com>
2024-11-05 16:56:19 +0100

    Update region code to be unique

--- oc_lib/models/region.py
+++ oc_lib/models/region.py
@@ -6,6 +6,6 @@ class Region(db.Model, Repository):
     __tablename__ = 'region'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
-    code = db.Column(db.Integer)
+    code = db.Column(db.Integer, unique=True)
 
     localites = db.relationship("localite", backref="region", uselist=True)
