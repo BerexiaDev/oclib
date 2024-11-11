7a81ea706717762b7e63c7f0855d8973df970bbd
Karim Bourass <bourasskarim9@gmail.com>
2024-11-11 12:55:28 +0100

    Define columns for BackupJob model attributes

--- oc_lib/models/backup_job.py
+++ oc_lib/models/backup_job.py
@@ -5,7 +5,8 @@ from oc_lib.repository import Repository
 class BackupJob(db.Model, Repository):
     __tablename__ = "backup_jobs"
     
-    backup_time= None
-    job_id=None
-    status= None
-    created_on= None
+    id = db.Column(db.Integer, primary_key=True)
+    backup_time = db.Column(db.DateTime, nullable=False)
+    job_id = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False)
