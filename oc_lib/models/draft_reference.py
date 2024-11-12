c363e7828a4251f86f779e65038bc257add257a2
Karim Bourass <bourasskarim9@gmail.com>
2024-11-12 12:20:15 +0100

    update draft_reference

--- oc_lib/models/draft_reference.py
+++ oc_lib/models/draft_reference.py
@@ -3,14 +3,11 @@ from oc_lib.db import db
 
 
 class DraftReference(db.Model, Repository):
-  __tablename__ = 'draft_reference'
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  centre = db.Column(db.Integer, nullable=False)
-  ville = db.Column(db.String(50), nullable=False)
-  rc = db.Column(db.Integer)
-  raison_sociale = db.Column(db.String(100))
-  address = db.Column(db.String(200))
-  forme_juridique = db.Column(db.String(100))
-  capital_social = db.Column(db.Numeric(precision=20, scale=3))
-  date_creation = db.Column(db.Date, nullable=False)
-  date_radiation = db.Column(db.Date, nullable=True)
+    __tablename__ = 'draft_reference'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    centre = db.Column(db.Integer, nullable=False)
+    rc = db.Column(db.Integer, nullable=False)
+    date_creation = db.Column(db.Date, nullable=False)
+    status = db.Column(db.Integer, default=0)
+    old_value = db.Column(db.String(250))
+    new_value = db.Column(db.String(250))
