48e1be2dbf7785e63fb046651c5d2143ad5610c1
Karim Bourass <bourasskarim9@gmail.com>
2024-11-13 12:53:00 +0100

    Update table name for SynchronizationJob model to singular form

--- oc_lib/models/synchronization_job.py
+++ oc_lib/models/synchronization_job.py
@@ -3,9 +3,8 @@ from oc_lib.repository import Repository
 
 
 class SynchronizationJob(db.Model, Repository):
-    __tablename__ = "synchronization_jobs"
+    __tablename__ = "synchronization_job"
     
     id = db.Column(db.Integer, primary_key=True)
-    job_id = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(50), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
