400ea694baaaccccaf1b1c527bff822494d96e2d
Karim Bourass <bourasskarim9@gmail.com>
2024-11-13 12:44:45 +0100

    Rename BackupJob model to SynchronizationJob and update imports accordingly

--- oc_lib/models/synchronization_job.py
+++ oc_lib/models/synchronization_job.py
@@ -2,11 +2,10 @@ from oc_lib.db import db
 from oc_lib.repository import Repository
 
 
-class BackupJob(db.Model, Repository):
-    __tablename__ = "backup_jobs"
+class SynchronizationJob(db.Model, Repository):
+    __tablename__ = "synchronization_jobs"
     
     id = db.Column(db.Integer, primary_key=True)
-    backup_time = db.Column(db.DateTime, nullable=False)
     job_id = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(50), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
