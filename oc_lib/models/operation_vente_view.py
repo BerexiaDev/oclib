dd3f5c92e8d19ba13bfcf669637975d6ce15bee9
abdel jalil <abdeljalil.jabiri@berexia.com>
2024-11-22 17:27:23 +0100

    Change idf and idce type from string to integer

--- oc_lib/models/operation_vente_view.py
+++ oc_lib/models/operation_vente_view.py
@@ -2,6 +2,7 @@ from oc_lib.db import db
 from oc_lib.repository import Repository
 from sqlalchemy.dialects.postgresql import JSONB
 
+
 class OperationVenteView(db.Model, Repository):
     __tablename__ = 'operation_vente_view'
 
@@ -11,7 +12,7 @@ class OperationVenteView(db.Model, Repository):
     date_bordereau = db.Column(db.DateTime)
     categorie_op = db.Column(db.String(50))
     pm_registre_commerce = db.Column(db.Integer)
-    pm_raison_sociale =  db.Column(db.String(100))
+    pm_raison_sociale = db.Column(db.String(100))
     pm_centre = db.Column(db.Integer)
     categorie_pc = db.Column(db.Integer)
     poc_id = db.Column(db.Integer)
@@ -20,43 +21,45 @@ class OperationVenteView(db.Model, Repository):
     poc_adresse = db.Column(db.String(50), nullable=False)
     created_by = db.Column(db.String(240))
     date_creation = db.Column(db.DateTime)
-    beneficiaire_pm_qualite =  db.Column(db.Integer, nullable=False)
+    beneficiaire_pm_qualite = db.Column(db.Integer, nullable=False)
     beneficiaire_pp_qualite = db.Column(db.Integer, nullable=False)
     beneficiaire_final_pp_qualite = db.Column(db.Integer, nullable=False)
     sous_operation_label = db.Column(db.String, nullable=False)
     sous_operation_id = db.Column(db.Integer, nullable=False)
-    beneficiaire_pp_id = db.Column( db.Integer, nullable=False )
-    beneficiaire_pp_nature_piece = db.Column( db.String(50), nullable=False )
-    beneficiaire_pp_numero_piece = db.Column( db.String(50), nullable=False )
+    beneficiaire_pp_id = db.Column(db.Integer, nullable=False)
+    beneficiaire_pp_nature_piece = db.Column(db.String(50), nullable=False)
+    beneficiaire_pp_numero_piece = db.Column(db.String(50), nullable=False)
     beneficiaire_pp_nom = db.Column(db.String(120), nullable=False)
     beneficiaire_pp_prenom = db.Column(db.String(120), nullable=False)
-    beneficiaire_pp_nationalite = db.Column( db.String(120), nullable=True )
-    beneficiaire_final_pp_nature_piece = db.Column( db.String(50), nullable=False )
-    beneficiaire_final_pp_numero_piece = db.Column( db.String(50), nullable=False )
+    beneficiaire_pp_nationalite = db.Column(db.String(120), nullable=True)
+    beneficiaire_final_pp_nature_piece = db.Column(
+        db.String(50), nullable=False)
+    beneficiaire_final_pp_numero_piece = db.Column(
+        db.String(50), nullable=False)
     beneficiaire_final_pp_nom = db.Column(db.String(120), nullable=False)
     beneficiaire_final_pp_prenom = db.Column(db.String(120), nullable=False)
-    beneficiaire_pm_id= db.Column(db.Integer, nullable=False)
+    beneficiaire_pm_id = db.Column(db.Integer, nullable=False)
     beneficiaire_pm_registre_commerce = db.Column(db.Integer, nullable=False)
     beneficiaire_pm_centre = db.Column(db.Integer, nullable=False)
     beneficiaire_pm_raison_sociale = db.Column(db.String(100))
-    beneficiaire_pm_idce = db.Column(db.String(50))
-    numero_autorisation = db.Column(db.Integer, nullable=True) # Si pp dispose d'autorisation 
+    beneficiaire_pm_idce = db.Column(db.Integer)
+    # Si pp dispose d'autorisation
+    numero_autorisation = db.Column(db.Integer, nullable=True)
     statut = db.Column(db.Integer)
     devise_labels = db.Column(db.String(1000))
     montant_global = db.Column(db.Float)
     support_mad = db.Column(JSONB, nullable=False)
 
-    beneficiaire_pc_id = db.Column( db.Integer, nullable=False )
+    beneficiaire_pc_id = db.Column(db.Integer, nullable=False)
     beneficiaire_pc_numero_agrement = db.Column(db.String(50))
     beneficiaire_pc_nom_agence = db.Column(db.String(50))
     beneficiaire_pc_qualite = db.Column(db.Integer, nullable=False)
-    
+
     cancellation_reason = db.Column(db.String(240))
     cancelled_by = db.Column(db.String(240))
     date_cancellation = db.Column(db.DateTime)
-    
-    activation_complement_dotation_id = db.Column( db.Integer )
+
+    activation_complement_dotation_id = db.Column(db.Integer)
 
     # operation_vente_lien_parente = db.Column(db.String(240)) # dans le cas de PP
     # operation_vente_fonction_pp = db.Column(db.String(240)) # dans le cas de PM
-    
