3e17a8b159e58dca11150e60348feeac9cf35b70
SoufianeAmghar <soufiane35.live@gmail.com>
2024-11-23 16:36:56 +0100

    Removed unused boolean fields

--- oc_lib/models/declaration_ana.py
+++ oc_lib/models/declaration_ana.py
@@ -10,8 +10,6 @@ class DeclarationAna(db.Model, Repository):
     motif_manager = db.Column(db.String(50))
     created_by = db.Column(db.String(240), nullable=False, server_default="")
     validateurs = db.Column(db.ARRAY(db.String(240)))
-    valide_oc = db.Column(db.Boolean, default=False)
-    valide_manager_oc = db.Column(db.Boolean, default=False)
     statut = db.Column(db.Integer)
     decision = db.Column(db.Integer)
     
