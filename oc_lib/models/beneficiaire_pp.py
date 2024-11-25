4f10df270cbd424f9ab788e867600f91add15339
myrzanur <hakedhacked0@gmail.com>
2024-11-25 13:19:16 +0100

    reset changes

--- oc_lib/models/beneficiaire_pp.py
+++ oc_lib/models/beneficiaire_pp.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import validates
 
 from oc_lib.db import db
 from oc_lib.models.beneficiaire import Beneficiaire
-from oc_lib.utils.validators import validate_cni, validate_cim, validate_normal_pattern
+from oc_lib.utils.validators import validate_numero_piece
 
 
 class BeneficiairePp(Beneficiaire):
@@ -17,9 +17,4 @@ class BeneficiairePp(Beneficiaire):
 
     @validates('numero_piece')
     def validate_numero_piece_value(self, key, value):
-        if self.nature_piece == "CNI" and self.qualite in [5, 6]:
-            return validate_cni(key, value)
-        elif self.nature_piece == "CIM" and self.qualite == 4:
-            return validate_cim(key, value)
-        elif self.nature_piece == "Passport" and self.qualite == 3:
-            return validate_normal_pattern(key, value)
+        return validate_numero_piece(key, value)
