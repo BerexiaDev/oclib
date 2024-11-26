9ef6e7587214a0f392739b35264568a454b27516
ceddibao <bllllaraf55@gmail.com>
2024-11-26 11:55:28 -0500

    use validates deco to check for address attribut in cogerant.

--- oc_lib/models/co_gerant.py
+++ oc_lib/models/co_gerant.py
@@ -1,14 +1,20 @@
 from oc_lib.db import db
 from oc_lib.models.pp import Pp
 from oc_lib.utils.events_decorator import register_event_listeners, change_statut_pp_pm_listener
+from sqlalchemy.orm import validates
 
 @register_event_listeners
 @change_statut_pp_pm_listener
 class Cogerant(Pp):
     id = db.Column(db.Integer, db.ForeignKey("pp.id"), primary_key=True)
-    adresse = db.Column(db.String(100), nullable=False)
 
     scd_id = db.Column(db.Integer, db.ForeignKey("scd.id"))
     esd_id = db.Column(db.Integer, db.ForeignKey("esd.id"))
+
+    @validates("address")
+    def validate_address(self, key, address):
+        if not address:
+            raise ValueError("L'adress est obligatoir pour un cogerant.")
+        return address
 
     __mapper_args__ = {"polymorphic_identity": "cogerant"}
