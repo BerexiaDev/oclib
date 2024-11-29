60066db3323e89ad4ebf91cc4ce1bb813ec1a057
abdel jalil <abdeljalil.jabiri@berexia.com>
2024-11-29 16:36:50 +0100

    Move fields support MAD and devise from Authorized Operation to Sub Operation

--- oc_lib/models/sous_operation.py
+++ oc_lib/models/sous_operation.py
@@ -1,22 +1,31 @@
 from oc_lib.repository import Repository
 from oc_lib.db import db
 from oc_lib.utils.strings import date_now
+from sqlalchemy.dialects.postgresql import ARRAY
+
 
 class SousOperation(db.Model, Repository):
     """ Model pour paramétrage des sous opérations """
 
     id = db.Column(db.Integer, primary_key=True)
     type_operation = db.Column(db.Integer, nullable=False)
-    code = db.Column(db.String(4), nullable=False, unique= True) # code de la sous operation A###, V###, C###
-    code_statistique = db.Column(db.Integer, unique=True) # code statistique
+    # code de la sous operation A###, V###, C###
+    code = db.Column(db.String(4), nullable=False, unique=True)
+    code_statistique = db.Column(db.Integer, unique=True)  # code statistique
     label = db.Column(db.String, nullable=False)
-    statut = db.Column(db.Boolean,nullable=False, default=True)
-    date_activation=db.Column(db.Date, nullable=False, default = date_now())
-    date_desactivation=db.Column(db.Date, nullable=True)
-    nature_beneficiaire = db.Column(db.ARRAY(db.Integer), nullable=False, default=[])
-    nature_beneficiaire_final = db.Column(db.ARRAY(db.Integer), nullable=False, default=[])
-    beneficiaire_final_required = db.Column(db.Boolean, nullable=False, default=False)
+    statut = db.Column(db.Boolean, nullable=False, default=True)
+    date_activation = db.Column(db.Date, nullable=False, default=date_now())
+    date_desactivation = db.Column(db.Date, nullable=True)
+    nature_beneficiaire = db.Column(
+        db.ARRAY(db.Integer), nullable=False, default=[])
+    nature_beneficiaire_final = db.Column(
+        db.ARRAY(db.Integer), nullable=False, default=[])
+    beneficiaire_final_required = db.Column(
+        db.Boolean, nullable=False, default=False)
     attachements = db.Column(db.ARRAY(db.String), nullable=False, default=[])
+    support_mad = db.Column(ARRAY(db.Integer), nullable=False)
+    support_devise = db.Column(ARRAY(db.Integer), nullable=False)
 
     # many to one
-    cancel_deadline_id = db.Column(db.Integer, db.ForeignKey('cancel_deadline.id'))
+    cancel_deadline_id = db.Column(
+        db.Integer, db.ForeignKey('cancel_deadline.id'))
