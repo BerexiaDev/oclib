ba4f9631eb8f2738a4f1af7b14233b860766be26
ceddibao <bllllaraf55@gmail.com>
2024-12-09 14:20:42 -0500

    added notification log to init.

--- oc_lib/models/__init__.py
+++ oc_lib/models/__init__.py
@@ -74,8 +74,8 @@ from oc_lib.models.currency import Currency
 from oc_lib.models.bank_code import BankCode
 from oc_lib.models.region import Region
 from oc_lib.models.localite import Localite
-from oc_lib.models.notification_log import NotificationLog
 
+from oc_lib.models.notification_log import NotificationLog
 # Association tables
 from oc_lib.models.authorized_operation_lieu_implantation_association import authorized_operation_lieu_implantation_association
 from oc_lib.models.derogation_encaisse_poc_association import derogation_encaisse_poc_association
