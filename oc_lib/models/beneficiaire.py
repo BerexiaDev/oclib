3a88f0ef0dea2fffec928d9d0f41ed81905313ee
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2025-01-08 08:20:26 +0100

    Update beneficiaire.py

--- oc_lib/models/beneficiaire.py
+++ oc_lib/models/beneficiaire.py
@@ -6,6 +6,7 @@ class Beneficiaire(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     qualite = db.Column(db.Integer, nullable=False)
     nationalite = db.Column( db.String(120))
+    id_pays = db.Column( db.String(120))
 
     date_deactivation = db.Column(db.DateTime) # date of deactivation
     statut = db.Column(db.Boolean, default=True, nullable=False)  # True active, False inactive
@@ -14,7 +15,7 @@ class Beneficiaire(db.Model, Repository):
     modified_by = db.Column(db.String(150))
     motif_modification = db.Column(db.String(100))
 
-    # this will be the discriminator attribute
+    # this will be the discriminator attribute 
     type = db.Column(db.String(50))
 
     __mapper_args__ = {"polymorphic_identity": "beneficiaire", "polymorphic_on": type}
