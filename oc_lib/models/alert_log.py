5c9cbdf617b3ec74107e916f37296f0afbeee653
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2025-01-09 17:05:46 +0100

    Update alert_log.py

--- oc_lib/models/alert_log.py
+++ oc_lib/models/alert_log.py
@@ -8,8 +8,8 @@ class AlertLog(db.Model, Repository):
 
     id = db.Column(db.Integer, primary_key=True)
     pc_id = db.Column(db.Integer, nullable=True)
-    pc_agreement_number = db.Column(db.String(100), nullable=False)
-    pc_designation = db.Column(db.String(255), nullable=False)
+    pc_agreement_number = db.Column(db.String(100), nullable=True)
+    pc_designation = db.Column(db.String(255), nullable=True)
     triggered_by = db.Column(db.String(100), nullable=False)
     triggered_by_id = db.Column(db.Integer, nullable=False)
     date_creation = db.Column(db.DateTime, default=datetime.utcnow)
