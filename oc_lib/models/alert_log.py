b00d4e2435f83fd3b61e704089b1098fb73cb2be
abdel jalil <abdeljalil.jabiri@berexia.com>
2025-01-09 13:53:33 +0100

    Add Alert Log to oclib

---
+++ oc_lib/models/alert_log.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from oc_lib.db import db
+from oc_lib.repository import Repository
+
+
+class AlertLog(db.Model, Repository):
+    __tablename__ = 'alert_log'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pc_id = db.Column(db.Integer, nullable=False)
+    pc_agreement_number = db.Column(db.String(100), nullable=False)
+    pc_designation = db.Column(db.String(255), nullable=False)
+    triggered_by = db.Column(db.String(100), nullable=False)
+    date_creation = db.Column(db.DateTime, default=datetime.utcnow)
+
+    alert_code = db.Column(db.String(50), nullable=False)
+    alert_name = db.Column(db.String(255), nullable=False)
