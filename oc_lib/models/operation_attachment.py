f21a581c8ffd18dad11ae01075d3e0822c221c28
SoufianeAmghar <soufiane35.live@gmail.com>
2025-02-11 11:15:51 +0100

    Removed external commit

--- oc_lib/models/operation_attachment.py
+++ oc_lib/models/operation_attachment.py
@@ -6,7 +6,7 @@ class OperationAttachment(db.Model, Repository):
     
     id = db.Column(db.Integer, primary_key=True)
     designation = db.Column(db.String(240))
-    nom_fichier = db.Column(db.String(500), nullable=False, unique=True)
+    nom_fichier = db.Column(db.String(500), nullable=False)
     full_path= db.Column(db.String(500), nullable =False)
     file_extension = db.Column(db.String(5), nullable=False)
     date_creation = db.Column(db.Date, default=db.func.now())
