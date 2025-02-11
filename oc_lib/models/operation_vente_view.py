a7e1bdda1f7a972403500ea4c538011870ffb46b
SoufianeAmghar <soufiane35.live@gmail.com>
2025-02-11 12:28:17 +0100

    Removed unwanted adress field

--- oc_lib/models/operation_vente_view.py
+++ oc_lib/models/operation_vente_view.py
@@ -32,14 +32,12 @@ class OperationVenteView(db.Model, Repository):
     beneficiaire_pp_nom = db.Column(db.String(120), nullable=False)
     beneficiaire_pp_prenom = db.Column(db.String(120), nullable=False)
     beneficiaire_pp_nationalite = db.Column(db.String(120), nullable=True)
-    beneficiaire_pp_adresse = db.Column(db.String(120), nullable=True)
     beneficiaire_final_pp_nature_piece = db.Column(
         db.String(50), nullable=False)
     beneficiaire_final_pp_numero_piece = db.Column(
         db.String(50), nullable=False)
     beneficiaire_final_pp_nom = db.Column(db.String(120), nullable=False)
     beneficiaire_final_pp_prenom = db.Column(db.String(120), nullable=False)
-    beneficiaire_final_pp_adresse = db.Column(db.String(120), nullable=True)
     beneficiaire_pm_id = db.Column(db.Integer, nullable=False)
     beneficiaire_pm_registre_commerce = db.Column(db.Integer, nullable=False)
     beneficiaire_pm_centre = db.Column(db.Integer, nullable=False)
