3081b19a3ce2c903115c0183ca1fec219da2c438
AMGIdhem <mahdi.bouhouch7@gmail.com>
2025-03-21 14:31:01 +0000

    enum value

--- oc_lib/auth/auth_helper.py
+++ oc_lib/auth/auth_helper.py
@@ -19,7 +19,7 @@ class AuthHelper:
         poc = Poc.find_one(id=user.poc_id)
         if poc:
             user.poc_statut_activite = poc.statut_activite
-            if poc.statut_activite in [StatutActivite.RETIRE, StatutActivite.ANNULE]:
+            if poc.statut_activite in [StatutActivite.RETIRE.value, StatutActivite.ANNULE.value]:
                 return user, 403
 
         g.user = user
