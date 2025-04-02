a0dfba40aef3b30ed003e230455e8387587ff7d7
Akremi Khayreddine <48913326+akremi-khayreddine@users.noreply.github.com>
2025-04-02 14:11:24 +0100

    Update pm_document.py

--- oc_lib/models/pm_document.py
+++ oc_lib/models/pm_document.py
@@ -5,9 +5,15 @@ from sqlalchemy import text
 
 class PmDocument(db.Model, Repository):
     id = db.Column(db.Integer, primary_key=True)
+    
+    scd_id = db.Column(db.Integer, db.ForeignKey('scd.id'))
+    esd_id = db.Column(db.Integer, db.ForeignKey('esd.id'))
+    ep_id = db.Column(db.Integer, db.ForeignKey('ep.id'))
+    
     pm_id = db.Column(db.Integer, db.ForeignKey("pm.id"), nullable=True)
     pp_id = db.Column(db.Integer, db.ForeignKey("pp.id"), nullable=True)
     poc_id = db.Column(db.Integer, db.ForeignKey("poc.id"), nullable=True)
+    
     intitule_document = db.Column(db.String(1000), nullable=False)
     nom_fichier = db.Column(db.String(1000), nullable=False)
     date_creation = db.Column(db.Date, default=db.func.now())
