4f69e45eb08bd689c8f76128ec77e7bc969df83c
ceddibao <bllllaraf55@gmail.com>
2025-04-21 19:07:07 -0400

    add conditino to request path.

--- oc_lib/auth/decorator.py
+++ oc_lib/auth/decorator.py
@@ -5,11 +5,16 @@ from oc_lib.auth.auth_helper import AuthHelper
 
 
 IGNORE_PATHS = ["/", "/swagger.json"]
+NOTIFICATION_CREATION_PATH = "/notifications"
 
 def decode_jwt(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print(request.path)
         if request.path in IGNORE_PATHS or "swagger" in request.path:
+            return f(*args, **kwargs)
+        
+        elif request.path == NOTIFICATION_CREATION_PATH and request.headers.get('X-NotifCreation', "false") == "true":
             return f(*args, **kwargs)
         try:
             # Decode token and store it in the global scope
