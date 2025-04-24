a510c10e10d1b7a82fb9ef6a85fed7c60e1a8873
ceddibao <bllllaraf55@gmail.com>
2025-04-24 14:04:56 -0400

    add limi to rs in references.

--- oc_lib/models/reference.py
+++ oc_lib/models/reference.py
@@ -8,7 +8,7 @@ class Reference(db.Model, Repository):
   centre = db.Column(db.Integer, nullable=False)
   ville = db.Column(db.String(50), nullable=False)
   rc = db.Column(db.Integer)
-  raison_sociale = db.Column(db.String(100))
+  raison_sociale = db.Column(db.String(1000))
   address = db.Column(db.String(200))
   forme_juridique = db.Column(db.String(100))
   capital_social = db.Column(db.Numeric(precision=20, scale=3))
