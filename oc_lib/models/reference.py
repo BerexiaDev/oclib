41395688f1f7494dc62c5b094daa0b5fdecf8d6b
ceddibao <bllllaraf55@gmail.com>
2025-04-29 13:43:21 -0400

    add value

--- oc_lib/models/reference.py
+++ oc_lib/models/reference.py
@@ -10,7 +10,7 @@ class Reference(db.Model, Repository):
   rc = db.Column(db.Integer)
   raison_sociale = db.Column(db.String(1000))
   address = db.Column(db.String(1000))
-  forme_juridique = db.Column(db.String(200))
+  forme_juridique = db.Column(db.String(1000))
   capital_social = db.Column(db.Numeric(precision=20, scale=3))
   date_creation = db.Column(db.Date, nullable=False)
   date_radiation = db.Column(db.Date, nullable=True)
